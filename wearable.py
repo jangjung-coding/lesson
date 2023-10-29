e70b789aeff5f96aa10cd66bab88ccf039148547
JangJungwoo <jangisalive1650@gmail.com>
2023-10-29 22:00:14 +0900

    restart

--- wearable.py
+++ wearable.py
@@ -7,10 +7,15 @@ path = 'raw_data_Liu.csv'
 df = pd.read_csv(path)
 df["ID"].unique()
 
-subject3 = df[df["ID"]==3]
-subject3
+# 0.실내 환경만 남겨두기 Location: (1 = indoor; -1 = outdoor)
+df[df["location"]==1] #3297
+df[df["location"]==-1] #546
+
+df_indoor = df[df["location"]==1]
+
+df_indoor
 
 # 1.개인별 최적 온도 찾기를 위해 데이터 개인별로 나누기(14명)
-for i in df["ID"].unique():
-    subject = df[df["ID"]==i]
+for i in df_indoor["ID"].unique():
+    subject = df_indoor[df_indoor["ID"]==i]
     subject.to_csv("subject"+str(i)+".csv")
