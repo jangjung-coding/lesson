58686f2d3c72b3254f5b609d5a1d6f4dd673483f
JangJungwoo <jangisalive1650@gmail.com>
2023-10-29 21:39:53 +0900

    devideSubject

--- wearable.py
+++ wearable.py
@@ -5,4 +5,12 @@ import matplotlib.pyplot as plt
 path = 'raw_data_Liu.csv'
 
 df = pd.read_csv(path)
-df.info()
+df["ID"].unique()
+
+subject3 = df[df["ID"]==3]
+subject3
+
+# 1.개인별 최적 온도 찾기를 위해 데이터 개인별로 나누기(14명)
+for i in df["ID"].unique():
+    subject = df[df["ID"]==i]
+    subject.to_csv("subject"+str(i)+".csv")
